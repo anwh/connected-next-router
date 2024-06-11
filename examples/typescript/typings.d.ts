c2be20bfd24e88c0aa239cc65f5c828a4f39b34d
anwh <enigm0x61@gmail.com>
2024-06-11 08:44:20 +0200

    remove unused files

--- examples/typescript/typings.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Store } from 'redux';
-import { NextPageContext } from 'next';
-import { AppContext } from 'next/app';
-import { RouterState } from '../../types';
-export interface State {
-  router: RouterState
-}
